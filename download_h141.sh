85f386644f0b4484fb8145ee1644bb3a272b4718
railgunpon <344012670@qq.com>
2025-05-14 23:04:50 +0800

    Update download_h141.sh

--- download_h141.sh
+++ download_h141.sh
@@ -4,7 +4,7 @@ echo "🔗 Connecting to H-SAF FTP to download h141_2008010100_R01.nc ..."
 
 lftp -u "344012670@qq.com","wangruijie62293" ftp://ftphsaf.meteoam.it <<EOF
 set ftp:passive-mode on
-get /products/h141/2008/01/h141_2008010100_R01.nc
+get /H141/2008/01/h141_2008010100_R01.nc
 bye
 EOF
 
